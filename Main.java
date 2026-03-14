3b1a49448d59dbe172c138ee52b04bf2306f81be
Mateusz Ammol <s32948@pjwstk.edu.pl>
2026-03-14 17:17:33 +0100

    Prosze na to nie patrzec

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-       Scanner scan=new Scanner(System.in);
-       System.out.println("Daj liczby rozdzielone spacja");
-       String line=scan.nextline();
-       String[] parts=line.split(" ");
-    }
-}
