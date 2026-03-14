902d535797ee846e5f2762b77aef02e6152b1c89
Mateusz Ammol <s32948@pjwstk.edu.pl>
2026-03-14 16:40:13 +0100

    Zad2 1/4 dodanie StatisticsHelper.java

---
+++ StatisticsHelper.java
@@ -0,0 +1,9 @@
+public class StatisticsHelper{
+    public double average(int[] numbers){
+        int sum=0:
+        for(int n :numbers){
+            sum+=n;
+        }
+        return (double) sum/ numbers.length;
+    }
+}
