a29007aef55691629e66c0c784c3163901b5054e
Mateusz Ammol <s32948@pjwstk.edu.pl>
2026-03-14 16:55:52 +0100

    Zad2 (2/4) dodanie obsluge danych wejsciowych

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       Scanner scan=new Scanner(System.in);
+       System.out.println("Daj liczby rozdzielone spacja");
+       String line=scan.nextline();
+       String[] parts=line.split(" ");
+    }
+}
